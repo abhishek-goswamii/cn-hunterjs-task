73336508963e7d00c9852745fbf0f71484fc887c
abhishek-goswamii <abi.xa12@gmail.com>
2023-08-15 16:03:49 +0530

    added hash

--- script/script.js
+++ script/script.js
@@ -1,23 +1,25 @@
 // var MD5 = require("crypto-js/md5");
 // const name = MD5(
 //   1 +
-//     "43398cd2c921ac523b546d6d31791fb015300573" +
-//     "8fc6f978258fe23fb428f93d3a84e90c"
+//     "8321d184952db4c7060109f228b56342d84a5e7c" +
+//     "a081b4482f155e13058ec86072ba6760"
 // ).toString();
 // console.log(name);
+// return
+//generating hash for api call
 
-var response; 
-var favItem = []; 
+var response;
+var favItem = [];
 var getfavItem = JSON.parse(localStorage.getItem("favItem"));
 
 favItem = getfavItem ? [...getfavItem] : [];
 
 const fetchApiHandeler = async () => {
-  let searchItem = document.getElementById("searchItem").value; 
+  let searchItem = document.getElementById("searchItem").value;
 
-  let url = `https://gateway.marvel.com:443/v1/public/characters?name=${searchItem}&ts=1&apikey=8fc6f978258fe23fb428f93d3a84e90c&hash=6f9f5d9b79f33bf48fe66627f30100c1`;
-  let body = await fetch(url); 
-  response = await body.json(); 
+  let url = `https://gateway.marvel.com:443/v1/public/characters?name=${searchItem}&ts=1&apikey=a081b4482f155e13058ec86072ba6760&hash=a94f480241098e7daa6c040fc67673f3`;
+  let body = await fetch(url);
+  response = await body.json();
 
   if (response.code == 409) {
     document.getElementById("main").style.display = "none";
